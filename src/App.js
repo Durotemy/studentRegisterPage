5e829b5fa19f9eb07620a56c6f6520e445fcfa7f
durotemy <durotemy@gmail.com>
2023-03-03 22:19:05 +0100

    final workdone

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import StudentDataTable from "./components/StudentDataTable";
+const  App = ()=> {
+  return (
+    <div className="">
+      <StudentDataTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// const Input = () => {
+//   return (
+//     <div className="mx-auto">
+//       <input
+//         type="text"
+//         id="large-input"
+//         className=" text-black mx-auto block w-80 h-16  p-4 rounded-lg bg-gray-50 sm:text-md  dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black  "
+//         placeholder="Search"
+//       />
+//     </div>
+//   );
+// };
+
+// export default Input;
